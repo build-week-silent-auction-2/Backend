c4a4c93940d9f23f8400d5fee89292b825b73c43
Michael <40902786+mtrew2015@users.noreply.github.com>
2019-11-16 02:04:43 -0500

    update

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
-
+let pg = require('pg')
+const dbConnection = process.env.DATABASE_URL 
 
 module.exports = {
 
@@ -58,12 +59,13 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: pg,
     connection: {
-      database: 'postgres://vbeqkcjqmcnyus:b43e5fefa4e9ff57f5d6b0ede0854c60149be7575b18fa4a0a90d6f9eb6bf7ba@ec2-174-129-253-144.compute-1.amazonaws.com:5432/dfebhbvll0v6jg',
+      database: dbConnection,
     },
     migrations: {
-      directory: './data/migrations'
+      directory: './data/migrations',
+      tableName: 'dbmigrations'
     },
     seeds: {
       directory: './data/seeds'
